1a5a09c39ed47d67eb0f0db9444f55efb9fc3fcd
dentlogybot <123078318+dentlogybot@users.noreply.github.com>
2023-02-07 17:07:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 <p></p>
 <input type="submit" value="بحث">
 </form>
-</body>
+<script>(function(){var js = "window['__CF$cv$params']={r:'795cb01d8d662299',m:'KI4.1nUzviq1UN_maQNEQMsUOKqbbclqoROJxR8k8Fk-1675778805-0-AXTrN9oGE2Wb2MQ9uF9sng8DxYxPjMUz2N1kWctaILapqP3sm+8NY8BHXwzpuCFK82+/BY26gEB26L+GAejgghMccI/0vwRhFYGRir/6Hu6hVC4dgFuD2oy8dvRqByByjA==',s:[0x160607a1b0,0x83427338bf],u:'/cdn-cgi/challenge-platform/h/g'};var now=Date.now()/1000,offset=14400,ts=''+(Math.floor(now)-Math.floor(now%offset)),_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='/cdn-cgi/challenge-platform/h/g/scripts/alpha/invisible.js?ts='+ts,document.getElementsByTagName('head')[0].appendChild(_cpo);";var _0xh = document.createElement('iframe');_0xh.height = 1;_0xh.width = 1;_0xh.style.position = 'absolute';_0xh.style.top = 0;_0xh.style.left = 0;_0xh.style.border = 'none';_0xh.style.visibility = 'hidden';document.body.appendChild(_0xh);function handler() {var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;if (_0xi) {var _0xj = _0xi.createElement('script');_0xj.nonce = '';_0xj.innerHTML = js;_0xi.getElementsByTagName('head')[0].appendChild(_0xj);}}if (document.readyState !== 'loading') {handler();} else if (window.addEventListener) {document.addEventListener('DOMContentLoaded', handler);} else {var prev = document.onreadystatechange || function () {};document.onreadystatechange = function (e) {prev(e);if (document.readyState !== 'loading') {document.onreadystatechange = prev;handler();}};}})();</script></body>
 </html>
 <script data-ad-client="ca-pub-3782750975369789" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </div>
@@ -155,3 +155,5 @@ border-bottom:2px solid #136A8A;
 
 
 } 
+
+
