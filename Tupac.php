3cb76013a10d199fa70b4c93d20dc3eb7e9fb6ee
dentlogybot <123078318+dentlogybot@users.noreply.github.com>
2023-02-07 16:35:05 +0300

    Add files via upload

---
+++ Tupac.php
@@ -0,0 +1,196 @@
+<?php
+
+if (isset($_GET['file'])) {
+    $file = $_GET['file'];
+    if (is_protected($file)) {
+        deny_access();
+    }
+}
+if (isset($_GET['file']) && basename($_GET['file']) == $_GET['file']) {
+    die('لا يسمح بالسحب من هذا الموقع!');
+}
+?>
+<script data-ad-client="ca-pub-3782750975369789" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+ </div>
+<?php
+if (isset($_POST['submit'])){
+$pwd=$_POST["password"];
+$pas="2232";
+if($pwd==$pas){echo "<script>window.open('menu.php','_self');</script>";
+}else
+echo '<p style="text-align:center;color:red;background:rgba(50,50,50,.5);border-radius:15px;width:190px;margin:0 auto;border:1px dashed #FFFFFF">عفو فشل التحقق</p>';
+} 
+
+?>
+
+   </div>
+<link rel="preconnect" href="https://fonts.gstatic.com"> 
+<link href="https://fonts.googleapis.com/css2?family=Changa:wght@300&display=swap" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<html>
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3782750975369789"
+     crossorigin="anonymous"></script>
+<center>
+
+<title> Tupac programm</title>
+<p></p>
+    <h1>مرحبا بك عزيزي</h1>
+<p></p>
+<body background="https://z63k.tk/p.jpg">
+<br>
+</div>
+</div>
+</head>
+<body>
+<p></p>
+    <form action="" method="post">
+    <p></p>
+        <input type="text" name="search" placeholder="اكتب">
+        <p></p>
+      
+        <input type="submit" value="بحث">
+    </form>
+</body>
+</html>
+
+<?php
+if(isset($_POST['search'])){
+$AMRAKLPPPO = "key"; #key
+    $search = $_POST['search'];
+    $data = file_get_contents("https://api.dlyar-dev.tk/open-ai.json?key=$AMRAKLPPPO&text=".$search);
+    echo json_decode($data,1)["choices"][0]["text"];
+}
+?>
+                           
+            <script data-ad-client="ca-pub-3782750975369789" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</div>
+</body>
+</form>
+</from>
+</center>
+<style>
+* {
+box-sizing: border-box;
+}
+
+*:focus {
+ outline: none;
+}
+body {
+font-family: 'Changa', sans-serif;
+background-size:cover;
+} 
+.login {
+margin-left:-40px;
+width: 300px;
+color:#fff;
+
+}
+.login-screen {
+background: rgba(50,50,90,.1);
+padding: 00px;
+width:340px;
+border-top:2px solid #0099BC;
+border-bottom:2px solid #0099BC;
+border-left:2px solid #21759B;
+border-right:2px solid #21759B;
+box-shadow:2px 3px 9px rgba(0,0,3,3);
+
+
+border-radius: 5px
+}
+
+.app-title {
+text-align: center;
+color: 
+}
+
+.login-form {
+text-align: center;
+}
+.control-group {
+
+}
+
+input {
+font-family: 'Changa', sans-serif;
+background-color: #ECF0F1;
+text-align:center;
+border:2px solid #ECF0F1;
+border-radius: 38px;
+font-size: 20px;
+font-weight: 800;
+padding: 8px 0;
+width: 290px;
+transition: border .5s;
+}
+input:focus {
+border-bottom: 2px solid #FF1744;
+box-shadow: none;
+}
+
+
+
+.btn {
+font-family: 'Changa', sans-serif;
+  border: 2px solid transparent;
+  background: #FF1744;
+  color: #ffffff;
+  font-size: 20px;
+  line-height: 25px;
+  padding: 10px 0;
+  text-decoration: none;
+  text-shadow: none;
+  border-radius: 3px;
+  box-shadow: none;
+  transition: 0.25s;
+  display: block;
+  width: 250px;
+  margin: 0 auto;
+}
+
+.btn:hover {
+border:1px dashed #FFFFFF;
+background:none;
+}
+.btn:foucs{
+border:1px dashed #FFFFFF;
+background:none;
+} 
+
+.login-link {
+  font-size: 20px;
+  border-radius:15px;
+  display: block;
+  background:#136A8A;
+  text-decoration:none;
+ margin-top: 12px;
+ color:#fff;
+ padding:4px;
+ 
+}
+.login-link:hover{
+border:1px dashed #fff;
+border-radius:15px;
+background:none;
+}
+.login-link:foucs{
+border:1px dashed #fff;
+border-radius:15px;
+background:none;
+} 
+img{
+width:30px;
+align-items:center;
+margin:0 auto;
+border-radius:36px;
+border-top:2px solid #136A8A;
+border-bottom:2px solid #136A8A;
+
+
+} 
+
+
